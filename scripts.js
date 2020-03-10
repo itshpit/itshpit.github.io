86709a413a81890d8220aec4ed23d1dca5f8eb7b
Stacie Shpit <56196026+itshpit@users.noreply.github.com>
2020-03-10 15:16:54 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+    $('.post-wrapper').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        nextArrow: $('.next'),
+        prevArrow: $('.prev'),
+      });
+});
+
